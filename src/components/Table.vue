1932b92907dc8c9bbed81f9ba7786203a2342092
moracabanas <moracabanas@live.com>
2018-11-07 00:24:48 +0100

    - Changed 'deliveries' variable to 'shipments' - Added some columns to match duties table - Added custom fields with key. label and custom evaluated properties. - Removed 'items' array as not needed eanymore.

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-table striped hover :items="deliveries"></b-table>
+  <b-table striped hover :items="shipments" :fields="fields"></b-table>
 </template>
 
 
@@ -9,16 +9,46 @@ export default {
   name: 'Table',
   data() {
     return {
-      deliveries: [],
+      fields: [
+        {
+          key: 'import_date',
+          label: 'Date',
+          sortable: true
+        },
+        {
+          key: 'MASTER_PARCEL_ID',
+          label: 'Shipment',
+          sortable: false
+        },
+        {
+          key: 'SHIPPER_COUNTRY',
+          label: 'From',
+          // Variant applies to the whole column, including the header and footer
+          //variant: 'danger'
+        },
+        {
+          key: 'CONSIGNEE_COUNTRY',
+          label: 'To'
+        },
+        {
+          key: 'ARTICLE_DESC',
+          label: 'Description',
+          sortable: true
+        },
+        {
+          key: 'CONSIGNEE_SIRET',
+          label: 'B2B',
+          formatter: value => {return (value ? 'B2B' : '') }
+        },
+      ],
+      shipments: [],
       errors: [],
-      items: [
-      ]
     }
   },
   mounted() {
     axios
       .get('https://duties.dpd.com.pl/?option=com_duties&task=api.getShipments')
-      .then(response => (this.deliveries = response.data))
+      .then(response => (this.shipments = response.data))
       .catch(error => {this.errors.push(error)})
       
     
