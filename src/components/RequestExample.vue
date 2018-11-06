6e099cdf6a76d6985d5a8ce927533725913bca24
moracabanas <moracabanas@live.com>
2018-11-06 16:13:17 +0100

    Example to use axios and Vue directives

---
+++ src/components/RequestExample.vue
@@ -0,0 +1,33 @@
+<template>
+      <ul>
+        <li v-for="cosa of cosas">
+          <p><strong>{{cosa.id}}</strong></p>
+          <p>{{cosa.ARTICLE_DESC}}</p>
+        </li>
+      </ul>
+</template>
+
+
+<script>
+
+export default {
+  name: 'RequestExmaple',
+  data() {
+    return {
+      cosas: [],
+      errors: []
+    }
+  },
+  mounted() {
+    axios
+      .get('https://duties.dpd.com.pl/?option=com_duties&task=api.getShipments')
+      .then(response => (this.cosas = response.data))
+      .catch(error => {this.errors.push(error)})
+    
+      //disabled console log  .catch(error => console.log(error)) // eslint-disable-line no-console
+  }
+}
+</script>
+
+<style>
+</style>
