273700ea569883be57a523a4822ba3bc018393ae
Alberto <moracabanas@live.com>
2018-12-05 00:46:42 +0200

    first commit - Filters to proper uppercase names - CONSIGNEE_NAME_1 & MASTER_PARCEL_ID reactivity binding - Cards and Style isolated

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
   <div id="app">
-    <Navbar/>
     <div class="container-fluid">
-      <div class="row-fluid">
-        <Table/>
-      </div>
+      <MobileDetails/>
+      <div class="row-fluid"></div>
     </div>
-
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import Table from './components/Table.vue';
+import MobileDetails from "./components/MobileDetails.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Navbar,
-    Table
+    MobileDetails
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/MobileDetails.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <br>
+    <h3 id="name">Hello {{shipment.CONSIGNEE_NAME_1 | formatName}}</h3>
+
+    <b-card id="parcel" text-variant="black" title="Parcel Number" class="text-center">
+      <h4>{{shipment.MASTER_PARCEL_ID}}</h4>
+    </b-card>
+
+    <br>
+    <b-card bg-variant="danger" text-variant="white" title class="text-center">
+      <h5>Details information of the order / parcel (description / sender)</h5>
+    </b-card>
+    <br>
+    <b-card id="instructions" text-variant="white" title class="text-center">
+      <h5>Instructions to get your parcel from UK</h5>
+    </b-card>
+    <br>
+    <b-card bg-variant="secondary" text-variant="white" title class="text-center">
+      <h5>Instructions to get your parcel from UK</h5>
+    </b-card>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      id: 18009531582000,
+      shipment: [],
+      errors: []
+    };
+  },
+  methods: {
+    /*
+    formatName(name) {
+      return "<span style='text-transform: capitalize'>" + name + "</span>";
+
+      
+      var splittedName = lowerName.split(" "),
+        firstName = splittedName[0],
+        lastName = splittedName[splittedName.length - 1];
+      console.log(lowerName);
+      console.log(splittedName);
+      console.log(firstName);
+      console.log(lastName);
+      var upperName = splittedName.toUpperCase().join(" ");
+      console.log(upperName);
+      }*/
+  },
+  filters: {
+    formatName: function titleCase(str) {
+      if (!str) return "";
+      var words = str.toLowerCase().split(" ");
+      for (var i = 0; i < words.length; i++) {
+        var j = words[i].charAt(0).toUpperCase();
+        words[i] = j + words[i].substr(1);
+      }
+      return words.join(" ");
+    },
+    capitalize: function(name) {
+      if (!name) return "";
+      name = name.toString();
+      name = name.toLowerCase();
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+  },
+  mounted() {
+    axios
+      .get(
+        "https://duties.dpd.com.pl/?option=com_duties&task=api.getShipment&shipment_id=" +
+          this.id
+      )
+      .then(response => {
+        console.log(response.data);
+        this.shipment = response.data;
+        //console.log(this.$route.query.id);
+      })
+      .catch(error => {
+        this.errors.push(error);
+      });
+
+    //disabled console log  .catch(error => console.log(error)) // eslint-disable-line no-console
+  }
+};
+</script>
+
+<style scoped>
+#parcel {
+  background-color: rgb(179, 179, 179);
+}
+
+#instructions {
+  background-color: rgb(129, 24, 46);
+}
+
+#name {
+  text-align: center;
+}
+</style>
